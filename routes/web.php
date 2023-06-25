e559b4e141ad5fdad3c4c9a1be6de83be661c545
bayscode <muhamadiqbaladityaputra@gmail.com>
2023-06-25 20:19:25 +0700

    Membuat design halaman presensi gps dan foto selfie

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PresensiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +15,17 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('auth.login');
+Route::middleware(['guest:karyawan'])->group(function () {
+    Route::get('/', function () {
+        return view('auth.login');
+    })->name('login');
+    Route::post('/proseslogin', [AuthController::class, 'proseslogin']);
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
-Route::post('/proseslogin', [AuthController::class, 'proseslogin']);
+Route::middleware(['auth:karyawan'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/proseslogout', [AuthController::class, 'proseslogout']);
+
+    //Presensi
+    Route::get('/presensi/create', [PresensiController::class, 'create']);
+});
