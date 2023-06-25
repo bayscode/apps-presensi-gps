f2c00a30d0a12fed55b16b005b7860b828ef8355
bayscode <muhamadiqbaladityaputra@gmail.com>
2023-06-25 22:13:38 +0700

    Menyimpan data presensi ke database

--- resources/views/presensi/create.blade.php
+++ resources/views/presensi/create.blade.php
@@ -41,9 +41,15 @@
 
     <div class="row">
         <div class="col">
-            <button id="takeabsen" class="btn btn-primary btn-block">
-                <ion-icon name="camera-outline"></ion-icon> Absen Masuk
-            </button>
+            @if ($cek > 0)
+                <button id="takeabsen" class="btn btn-danger btn-block">
+                    <ion-icon name="camera-outline"></ion-icon> Absen Pulang
+                </button>
+            @else
+                <button id="takeabsen" class="btn btn-primary btn-block">
+                    <ion-icon name="camera-outline"></ion-icon> Absen Masuk
+                </button>
+            @endif
         </div>
     </div>
 
@@ -85,5 +91,38 @@
         }
 
         function errorCallback() {}
+        $("#takeabsen").click(function(e) {
+            Webcam.snap(function(uri) {
+                image = uri;
+            });
+            var lokasi = $("#lokasi").val();
+            $.ajax({
+                type: 'POST',
+                url: '/presensi/store',
+                data: {
+                    _token: "{{ csrf_token() }}",
+                    image: image,
+                    lokasi: lokasi
+                },
+                cache: false,
+                success: function(respond) {
+                    var status = respond.split("|");
+                    if (status[0] == "success") {
+                        Swal.fire({
+                            title: 'Berhasil!',
+                            text: status[1],
+                            icon: 'success',
+                        })
+                        setTimeout("location.href='/dashboard'", 3000);
+                    } else {
+                        Swal.fire({
+                            title: 'Gagal!',
+                            text: 'Maaf anda gagal absen!',
+                            icon: 'error',
+                        })
+                    }
+                }
+            });
+        });
     </script>
 @endpush
