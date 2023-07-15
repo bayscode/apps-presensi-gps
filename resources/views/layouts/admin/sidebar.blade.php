8fe69ef135a6fcb726dc742fbf8a8b0760a71bb0
bayscode <muhamadiqbaladityaputra@gmail.com>
2023-07-15 14:08:16 +0700

    Membuat halaman departemen(CRUD)

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -249,6 +249,9 @@
                                 <a class="dropdown-item" href="/karyawan">
                                     Data Karyawan
                                 </a>
+                                <a class="dropdown-item" href="/departemen">
+                                    Data Departemen
+                                </a>
                             </div>
                         </div>
                     </div>
